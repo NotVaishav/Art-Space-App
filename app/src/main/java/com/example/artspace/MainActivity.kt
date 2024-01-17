258d61e56c5627ea1e9375c821929206da6ab6be
Vaishav Dhepe <vaishav@Vaishavs-MacBook-Air.local>
2024-01-16 23:19:34 -0400

    added static image, layout

--- app/src/main/java/com/example/artspace/MainActivity.kt
+++ app/src/main/java/com/example/artspace/MainActivity.kt
@@ -3,13 +3,35 @@ package com.example.artspace
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.artspace.ui.theme.ArtSpaceTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +44,103 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    ArtSpaceLayout()
                 }
             }
         }
     }
 }
 
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun ArtSpaceLayout(modifier: Modifier = Modifier) {
+    Column(
         modifier = modifier
-    )
+            .fillMaxSize()
+            .padding(vertical = 4.dp, horizontal = 20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        ArtPreview(modifier.padding(top = 50.dp))
+        Spacer(modifier = modifier.height(50.dp))
+        ArtInformation()
+        ArtButtons()
+    }
+}
+
+@Composable
+fun ArtPreview(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(450.dp)
+            .shadow(10.dp)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.arts_1),
+            contentScale = ContentScale.Crop,
+            contentDescription = null,
+            modifier = modifier.padding(horizontal = 50.dp)
+        )
+    }
+}
+
+@Composable
+fun ArtInformation(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(horizontal = 15.dp)
+            .background(color = Color.LightGray)
+    ) {
+        Column(
+            modifier = modifier.padding(20.dp)
+        ) {
+            Text(
+                text = "Name of the art piece",
+                fontSize = 26.sp,
+            )
+            Row {
+                Text(
+                    text = "Author Name",
+                    modifier.padding(end = 3.dp),
+                    fontWeight = FontWeight.Bold
+                )
+                Text(text = "(2021)")
+            }
+        }
+    }
+}
+
+@Composable
+fun ArtButtons(modifier: Modifier = Modifier) {
+    Row(
+        modifier = modifier
+            .padding(10.dp)
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceEvenly
+    ) {
+        Button(
+            onClick = { /*TODO*/ },
+            colors = ButtonDefaults.buttonColors(containerColor = Color.Blue),
+            modifier = modifier.padding(horizontal = 10.dp)
+        ) {
+            Text(text = "Previous")
+        }
+        Button(
+            onClick = { /*TODO*/ },
+            colors = ButtonDefaults.buttonColors(containerColor = Color.Blue),
+            modifier = modifier.padding(horizontal = 10.dp)
+        ) {
+            Text(text = "Next")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ArtSpaceTheme {
-        Greeting("Android")
+        ArtSpaceLayout()
     }
 }
